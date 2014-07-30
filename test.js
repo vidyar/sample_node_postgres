925e76e97a0b30b119b9603e6e0ecfe03c73abdc
vidyaraghav <vidyac.raghav@gmail.com>
2014-07-30 12:30:58 +0530

    Update test.js

--- test.js
+++ test.js
@@ -23,7 +23,7 @@ describe('Postgres Database', function () {
   });
   it('should save a new name', function (done) {
     db('things')
-      .insert({ name: 'Johnson' })
+      .insert({ name: 'John' })
       .exec(function (err) {
         expect(err).to.equal(null);
         done();
@@ -33,7 +33,7 @@ describe('Postgres Database', function () {
     db('things')
       .select()
       .then(function (docs) {
-        expect(docs[0].name).to.equal('John');
+        expect(docs[0].name).to.equal('Johnson');
         done();
       });
   });

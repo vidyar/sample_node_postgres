61eb3c5d14d08f8a988b75368fc865b69b0fe12f
Vidya Raghavan <vidya@shippable.com>
2015-11-02 14:10:44 +0530

    Update test.js

--- test.js
+++ test.js
@@ -33,7 +33,7 @@ describe('Postgres Database', function () {
     db('things')
       .select()
       .then(function (docs) {
-        expect(docs[0].name).to.equal('Johnsoqn');
+        expect(docs[0].name).to.equal('Johnson');
         done();
       });
   });
